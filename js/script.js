5d3e1774b95bc8b205e0f9c2f30c62209f94b4a0
bePIRRO <manuel.pirro@gmail.com>
2021-08-30 12:33:47 +0200

    fix print user/contacts

--- js/script.js
+++ js/script.js
@@ -1,13 +1,11 @@
 console.log("test", Vue);
 
-console.log("Vue ready", Vue);
-
 Vue.config.devtools = true;
 
 var app = new Vue({
-    el: "#app",
-    data: {
-        data,
-    },
-    methods: {},
+  el: "#app",
+  data: {
+    data,
+  },
+  methods: {},
 });
--- js/users.js
+++ js/users.js
@@ -46,6 +46,7 @@ const data = {
           date: "20/03/2020 16:35:00",
           message: "Mi piacerebbe ma devo andare a fare la spesa.",
           status: "received",
+        },
       ],
     },
     {
